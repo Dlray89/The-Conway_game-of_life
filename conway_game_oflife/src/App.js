9fe554bf97b173f8e694fe0377132b06dce7f786
DAP <dlrayjr89@gmail.com>
2020-06-28 07:56:45 +0000

    styling added

--- conway_game_oflife/src/App.js
+++ conway_game_oflife/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Universe from './componets/Universal';
+import { TextField, Button, Slider, Dialog, DialogContent, DialogContentText, DialogActions, DialogTitle, Divider, List, ListItem, ListItemText } from "@material-ui/core"
 
 export default class Game extends Component {
     constructor(props) {
         super(props);
         this.state = {
             universe: new Universe(),
-            size: [90, 20],
+            size: [50, 20],
             gameRunning: false,
-            interval: 100
+            interval: 100,
+            open: false
         }
 
         this.handleColumnChange = this.handleColumnChange.bind(this);
@@ -40,10 +42,10 @@ export default class Game extends Component {
     handleColumnChange(event) {
         if (!this.state.gameRunning) {
             var actualSize = this.state.size;
-            if (event.target.value < 90)
+            if (event.target.value < 50)
                 actualSize[0] = event.target.value;
             else
-                actualSize[0] = 90;
+                actualSize[0] = 50;
 
             this.setState({
                 size: actualSize,
@@ -57,6 +59,24 @@ export default class Game extends Component {
         if (!this.state.gameRunning) {
             this.setState({
                 interval: event.target.value
+            })
+        }
+    }
+
+    handleOpen = () => {
+        if (!this.state.open) {
+            this.setState({
+                open: true
+            })
+
+        }
+
+    }
+
+    handleClose = () => {
+        if (this.state.open) {
+            this.setState({
+                open: false
             })
         }
     }
@@ -121,30 +141,88 @@ export default class Game extends Component {
     render() {
         return (
             <div className="worldContainer">
+
+                <div className="intro">
+                        <div>
+                            <h1>John Conway Game Of Life</h1>
+                            <p>
+                                 The Game of Life (sometimes known simply as Life) is an example of a cellular automaton and a zero-player game. It takes place on an infinite two-dimensional grid in which cells can be ‘on’ (alive) or ‘off’ (dead), and is defined by a set of rules that jointly determine the state of a cell given the state of its neighbours. Following specification of an initial configuration, patterns evolve over time across the grid requiring no further user input (thus ‘zero-player’)
+                            </p>
+
+                            <List>
+                                <p className='rules'>Rules</p>
+                                <ListItem button>
+                                    
+                                    <ListItemText style={{color: 'white', textAlign: 'center'}}>
+                                        A dead cell with exactly three live neighbors becomes a live cell (birth).
+                                    </ListItemText>
+                                </ListItem>
+                                <Divider style={{background: 'white'}}  />
+                                <ListItem button>
+                                    <ListItemText style={{color: 'white', textAlign: 'center'}}>
+                                        A live cell with two or three live neighbors stays alive (survival)
+                                    </ListItemText>
+                                </ListItem>
+
+                                 <Divider style={{background: 'white'}}  />
+                                <ListItem button>
+                                    <ListItemText style={{color: 'white', textAlign: 'center'}}>
+                                        In all other cases, a cell dies or remains dead (overcrowding or loneliness).
+                                    </ListItemText>
+                                </ListItem>
+                            </List>
+                           
+                        </div>
+
+                    </div>
+
+                    <div className='controls'>
+                        Controls:
+                    </div>
                 <div className="headerContainer">
+
+                    
                     <div className="headerInnerContainer">
-                        <label className="label">
-                            Rows:
-              <input className="input" type="text" value={this.state.size[1]} onChange={this.handleRowChange} />
-                        </label>
-                        <label className="label">
-                            Columns:
-              <input className="input" type="text" value={this.state.size[0]} onChange={this.handleColumnChange} />
-                        </label>
-                        <label className="label">
-                            Interval:
-              <input className="input" type="text" value={this.state.interval} onChange={this.changeInterval} />
-                        </label>
+                        <Button className='buttons' style={{background: 'linear-gradient(to bottom, #870000, #190a05)', color: 'white', border:'solid 1px red'}}variant='outlined' onClick={this.handleOpen}>Settings</Button>
+
+                        <Dialog open={this.state.open}>
+                            <DialogTitle>Settings</DialogTitle>
+                            <DialogContent>
+                                <DialogContentText>
+                                    <TextField  label='Rows' variant='outlined' className="input" type="text" value={this.state.size[1]} onChange={this.handleRowChange} />
+
+
+                                    <TextField style={{margin: '3% 0'}} label='Columns' variant='outlined' className="input" type="text" value={this.state.size[0]} onChange={this.handleColumnChange} />
+
+
+                                    <TextField label='Interval' variant='outlined' className="input" type="text" value={this.state.interval} onChange={this.changeInterval} />
+
+                                </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={this.handleClose}>Save</Button>
+                                <Button onClick={this.handleClose}>Cancel</Button>
+                            </DialogActions>
+                        </Dialog>
+
+                        <Button variant='outlined' className="buttons"  onClick={this.startGame}style={{background: 'linear-gradient(to bottom, #870000, #190a05)', color: 'white', border:'solid 1px red'}} >Start</Button>
+                        <Button style={{background: 'linear-gradient(to bottom, #870000, #190a05)', color: 'white', border:'solid 1px red'}} variant='outlined' className="buttons" onClick={this.stopGame}>Stop</Button>
+
+
                     </div>
-                    <div className="headerButtons">
-                        <button className="submit" onClick={this.startGame}>Start</button>
-                        <button className="submit" onClick={this.stopGame}>Stop</button>
-                    </div>
-          Generation: {this.state.universe.getGeneration()}
+                    
+                  
+
                 </div>
+                <Divider  style={{background: 'white', margin: '3% auto', width: '90%'}} />
+                <div className='generation'>
+                    Generation: {this.state.universe.getGeneration()}
+                </div>
+                
                 <div className="boardContainer">
                     {this.renderBoard()}
                 </div>
+
             </div>
         );
     }
@@ -153,7 +231,7 @@ export default class Game extends Component {
 class Cell extends Component {
     render() {
         return (
-            <div onClick={() => this.props.storeCell(this.props.position)} className={this.props.live ? "cellContainerLive" : "cellContainerDead"}></div>
+            <div onClick={() => this.props.storeCell(this.props.position)} className={this.props.live ? "color-change-2x" : "cellContainerDead"}></div>
         );
     }
 }
